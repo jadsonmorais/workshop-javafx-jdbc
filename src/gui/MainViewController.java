c0c0a559c166b1710f241c60e97c854ec7c55133
Jadson Morais <morais.jadsonluiz@gmail.com>
2021-02-07 12:15:00 -0300

    DepartmentList controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -43,7 +43,7 @@ public class MainViewController implements Initializable {
 	}
 
 	@Override
-	public void initialize(URL uri, ResourceBundle bd) {		
+	public void initialize(URL url, ResourceBundle bd) {		
 	}
 	
 	private synchronized void loadView(String absoluteName) {
